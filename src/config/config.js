e367af064c5b12b63661b26a388b8cfaadd2988f
Tholhen <cabiran.nicolas@gmail.com>
2021-09-23 11:19:59 +0200

    modification de l'URL de l'API

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 export const config = {
-	api_url: "http://nicolascab.ide.3wa.io:9005",
-	product_pict_url: "http://nicolascab.ide.3wa.io:9005/images/products/",
-	shop_opinion_pict_url: "http://nicolascab.ide.3wa.io:9005/images/opinions/shop/",
-	dailystand_detail_pict_url: "http://nicolascab.ide.3wa.io:9005/images/dailystand/",
+	api_url: "https://lecaille-test.herokuapp.com/",
+	product_pict_url: "https://lecaille-test.herokuapp.com/images/products/",
+	shop_opinion_pict_url: "https://lecaille-test.herokuapp.com/images/opinions/shop/",
+	dailystand_detail_pict_url: "https://lecaille-test.herokuapp.com/images/dailystand/",
 	emailRegEx: /^[a-z]+([\.\-_]?[a-z0-9]+)*@([a-z]{3,})+(\.[a-z]{2,3})+$/,
 	phoneRegEx: /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/
 }
