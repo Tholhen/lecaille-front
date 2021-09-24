5e349dc9222e0068821a5a1e0bc06854dfffbdfa
Tholhen <cabiran.nicolas@gmail.com>
2021-09-24 11:28:21 +0200

    test

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ export const config = {
 	api_url: "https://lecaille-test.herokuapp.com",
 	product_pict_url: "https://lecaille-test.herokuapp.com/images/products/",
 	shop_opinion_pict_url: "https://lecaille-test.herokuapp.com/images/opinions/shop/",
-	dailystand_detail_pict_url: "https://lecaille-test.herokuapp.com/images/dailystand/",
+	dailystand_detail_pict_url: "https://lecaille-test.herokuapp.com/images/dailystand/" ,
 	emailRegEx: /^[a-z]+([\.\-_]?[a-z0-9]+)*@([a-z]{3,})+(\.[a-z]{2,3})+$/,
 	phoneRegEx: /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/
 }
